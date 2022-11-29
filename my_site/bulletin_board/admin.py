12f7d553aecee35355807e53e08c047cc9ba28ee
DionVik <d.v.efremov@mail.ru>
2022-11-29 22:36:24 +0400

    Model category

--- my_site/bulletin_board/admin.py
+++ my_site/bulletin_board/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import User_profile
+from .models import Category
 from .models import Advertisment
 # Register your models here.
 admin.site.register(User_profile)
+admin.site.register(Category)
 admin.site.register(Advertisment)
 
--- my_site/bulletin_board/models.py
+++ my_site/bulletin_board/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-def picture_path(instance, file):
-    user_id = instance.id
-    return 'avatar/user-{}/{}'.format(user_id, file)
+#def picture_path(instance, file):
+    #user_id = instance.id
+    #return 'avatar/user-{}/{}'.format(user_id, file)
 
-def picture_path(instance, file):
-    user_id = instance.id
-    return 'picture/user-{}/{}'.format(user_id, file)
+#def picture_path(instance, file):
+    #user_id = instance.id
+    #return 'picture/user-{}/{}'.format(user_id, file)
 
 class User_profile(models.Model):
     name = models.CharField(max_length = 20)
@@ -15,11 +15,24 @@ class User_profile(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length = 11)
     birth_date = models.DateField(blank=True, null=True)
-    avatar = models.ImageField(upload_to='media/', blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name} {self.last_name}'    
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return f'{self.name}'    
     
 class Advertisment(models.Model):
     user = models.ForeignKey(User_profile, on_delete = models.CASCADE)
-    category = models.CharField(max_length = 100)
+    category = models.ForeignKey(Category, on_delete = models.SET_DEFAULT, default = None)
     title = models.CharField(max_length = 100)
-    content = models.TextField(blank=True)
-    picture = models.ImageField(upload_to=picture_path, blank=True)
+    content = models.TextField(max_length = 500)
+    picture = models.ImageField(upload_to='pictures/', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.user} {self.title}'    
