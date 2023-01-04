6d67e1543bfa44917ff04d6f80372ec06b187688
DionVik <d.v.efremov@mail.ru>
2023-01-04 16:57:33 +0400

    Add class CustomUserAdmin in admin.py for correct login

--- ads/accounts/admin.py
+++ ads/accounts/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 # Register your models here.
-admin.site.register(CustomUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'region', 'phone', 'email', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('phone', 'region', 'avatar')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('phone', 'region', 'avatar')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
